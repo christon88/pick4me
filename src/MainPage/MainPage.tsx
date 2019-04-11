3bca032506ce9cdf0c57aee7823ad802f028c4dd
christon88 <c.a.onsager@gmail.com>
2019-04-11 20:23:08 +0200

    Initial commit

---
+++ src/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainPage.scss";
+
+const MainPage = () => (
+  <div className="bgp-main-page">
+    <div className="bgp-log-in-button">Log In</div>
+    <div className="bgp-header">Header</div>
+    <div className="bgp-main-body">MainBody</div>
+  </div>
+);
+
+export default MainPage;
